8b93bf5dc33130bd4896a4e19794d5ff15b139d7
Anderson <andy.walter@hotmail.com>
2022-01-07 14:10:16 -0300

    Consertado bug do formulário

--- Desafio-7/frontend/src/app/components/form/form.component.ts
+++ Desafio-7/frontend/src/app/components/form/form.component.ts
@@ -1,7 +1,6 @@
-import { MatFormFieldControl } from '@angular/material/form-field';
 import { Component, OnInit} from '@angular/core';
 import { ServiceService } from '../services/service.service';
-import { FormControl, Validators} from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -10,36 +9,47 @@ import { FormControl, Validators} from '@angular/forms';
 })
 export class FormComponent implements OnInit {
 
-  nome = new FormControl(null, Validators.required);
-  email = new FormControl(null, [Validators.required, Validators.email]);
-  mensagem =  new FormControl(null, [Validators.required, Validators.minLength(5)]);
+  formulario: FormGroup = new FormGroup({
+    nome: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    mensagem: new FormControl(null, Validators.required)
+  })
+
 
   constructor(private service: ServiceService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
 
   }
 
   getErrorMessageNome() {
-    if (this.nome.hasError('required')) {
+    if (this.formulario.controls.nome.hasError('required')) {
       return 'You must enter your name';
     }
 
-    return this.nome.hasError('nome') ? 'Not a name' : '';
+    return this.formulario.hasError('nome') ? 'Not a name' : '';
   }
 
   getErrorMessageEmail() {
-    if (this.email.hasError('required')) {
+    if (this.formulario.controls.email.hasError('required')) {
       return 'You must enter your e-mail';
     }
 
-    return this.email.hasError('email') ? 'Not a valid e-mail' : '';
+    return this.formulario.hasError('email') ? 'Not a valid e-mail' : '';
   }
 
-  /*enviaMensagem(): void {
+  getErrorMessageMensagem() {
+    if (this.formulario.controls.mensagem.hasError('required')) {
+      return 'You must enter your message';
+    }
+
+    return this.formulario.hasError('mensagem') ? 'Not a valid message' : '';
+  }
+
+  enviaMensagem(): void {
     this.service.mensagem(this.formulario.value).subscribe(() =>{
       this.service.showMessage('Mensagem enviada!'),
       this.formulario.reset()
     })
-  }*/
+  }
 }
